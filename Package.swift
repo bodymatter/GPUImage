40a187292e9b7a9d8e40e10f72bde51d53f5ab66
Andy Yeung <andy@bodymatter.io>
2022-08-18 17:43:19 -0700

    Add product library

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GPUImage",
+            targets: ["GPUImage-iOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
